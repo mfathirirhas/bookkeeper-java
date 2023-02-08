7279caba203ebc3af4e97c81b033e5a783dd5d5c
mfathirirhas <mfathirirhas@gmail.com>
2023-02-08 19:46:02 +0700

    Fix kafka send

--- journal/src/main/java/com/journal/service/PublisherService.java
+++
@@ -1,8 +0,0 @@
-package com.journal.service;
-
-import com.journal.repo.models.DayBook;
-import io.reactivex.Completable;
-
-public interface PublisherService {
-    Completable publishDaybook(String topic, DayBook dayBook);
-}
